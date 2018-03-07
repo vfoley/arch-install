701f0fd3c4dc65661424ebcc8461bdfda6d88e40
Vernon Foley <vfoley@users.noreply.github.com>
2018-03-06 23:38:24 -0700

    Update sudoInstalls.sh

--- sudoInstalls.sh
+++ sudoInstalls.sh
@@ -3,7 +3,7 @@ hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
-echo "archvm" >> /etc/hostname
+echo "v-arch-one" >> /etc/hostname
 
 pacman -Syu
 pacman -S wicd grub --noconfirm --needed
